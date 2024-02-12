42c59c92ac1f8795d7e273b89bc88aa302b42345
iheb meftah <ihebmeftah666@gmail.com>
2024-02-13 00:05:54 +0100

    QUIZ ENDPOINT

---
+++ quiz_server/lib/src/endpoints/quiz_endpoint.dart
@@ -0,0 +1,49 @@
+import 'package:quiz_server/src/generated/protocol.dart';
+import 'package:serverpod/serverpod.dart';
+
+class QuizEndpoint extends Endpoint {
+  Future<List<Quiz>> getQuizes(Session s) async {
+    return Quiz.db.find(s);
+  }
+
+  Future<Quiz?> getOneQuiz(Session s, {required int id}) async {
+    return Quiz.db.findById(s, id);
+  }
+
+  Future<Quiz> createQuiz(
+    Session s, {
+    required String name,
+    required String description,
+  }) async {
+    return await Quiz.db.insertRow(
+        s,
+        Quiz(
+          name: name,
+          description: description,
+          status: Status.pending,
+        ));
+  }
+
+  Future<Quiz> updateQuizStatus(Session s,
+      {required int quizId, required Status status}) async {
+    var quiz = await Quiz.db.findById(s, quizId);
+    quiz!.status = status;
+    return await Quiz.db.updateRow(s, quiz);
+  }
+
+  Future<Quiz> updateQuiz(
+    Session s, {
+    required int quizId,
+    required String name,
+    required String description,
+  }) async {
+    var quiz = await Quiz.db.findById(s, quizId);
+    quiz!.name = name;
+    quiz.description = description;
+    return await Quiz.db.updateRow(s, quiz);
+  }
+
+  Future<int> deleteQuiz(Session s, Quiz quiz) async {
+    return await Quiz.db.deleteRow(s, quiz);
+  }
+}
