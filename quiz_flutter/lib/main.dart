2a9fd68f7852e4134f2b486f3651be8d1d4e60d9
iheb meftah <ihebmeftah666@gmail.com>
2024-02-12 22:35:42 +0100

    first commit

---
+++ quiz_flutter/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:quiz_client/quiz_client.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
+
+import 'app/routes/app_pages.dart';
+
+final Client client = Client('http://localhost:8080/')
+  ..connectivityMonitor = FlutterConnectivityMonitor();
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
+}
