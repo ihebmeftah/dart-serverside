b687ad536940ddcec58d00af668131372380cb3e
ihebmeftah <ihebmeftah666@gmail.com>
2024-02-29 18:39:47 +0100

    init

---
+++ lms_flutter/lib/app/app_client.dart
@@ -0,0 +1,29 @@
+import 'package:lms_client/lms_client.dart';
+import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
+
+late SessionManager sessionManager;
+late Client client;
+Future<void> appClientInit() async {
+  // The android emulator does not have access to the localhost of the machine.
+  // const ipAddress = '10.0.2.2'; // Android emulator ip for the host
+
+  // On a real device replace the ipAddress with the IP address of your computer.
+  const ipAddress = 'localhost';
+
+  // Sets up a singleton client object that can be used to talk to the server from
+  // anywhere in our app. The client is generated from your server code.
+  // The client is set up to connect to a Serverpod running on a local server on
+  // the default port. You will need to modify this to connect to staging or
+  // production servers.
+  client = Client(
+    'http://$ipAddress:8080/',
+    authenticationKeyManager: FlutterAuthenticationKeyManager(),
+  )..connectivityMonitor = FlutterConnectivityMonitor();
+
+  // The session manager keeps track of the signed-in state of the user. You
+  // can query it to see if the user is currently signed in and get information
+  // about the user.
+  sessionManager = SessionManager(caller: client.modules.auth);
+  await sessionManager.initialize();
+}
---
+++ lms_flutter/lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
