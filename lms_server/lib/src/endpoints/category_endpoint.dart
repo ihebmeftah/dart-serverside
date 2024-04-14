942d65b95b514681cf87844e6f7444ec626fc866
ihebmeftah <ihebmeftah666@gmail.com>
2024-04-14 16:04:45 +0100

    Refactor category endpoint methods and add update functionality
    - Refactored getCategoryById and checkCategoryByName methods for better error handling
    - Added updateCategory method to allow updating category name and description
    - Updated deleteCategory method to use getCategoryById for better consistency

--- lms_server/lib/src/endpoints/category_endpoint.dart
+++ lms_server/lib/src/endpoints/category_endpoint.dart
@@ -24,6 +24,55 @@ class CategoryEndpoint extends Endpoint {
     }
   }
 
+  Future<Category> getCategoryById(Session session, int id) async {
+    final int? userId = await session.auth.authenticatedUserId;
+    if (userId == null) {
+      throw AppException(
+          message: 'This request required authintification of admin',
+          errorType: ExceptionType.authenticationRequiredException);
+    }
+    final category = await Category.db.findById(session, id);
+    if (category == null || category.userId != userId) {
+      throw AppException(
+          message: 'Category not found', errorType: ExceptionType.notFound);
+    }
+    return category;
+  }
+
+  Future<void> checkCategoryByName(Session session, String name) async {
+    final int? userId = await session.auth.authenticatedUserId;
+    if (userId == null) {
+      throw AppException(
+          message: 'This request required authintification of admin',
+          errorType: ExceptionType.authenticationRequiredException);
+    }
+    final category = await Category.db.findFirstRow(
+      session,
+      where: (cat) => cat.name.ilike(name.trim()),
+    );
+    if (category != null) {
+      throw AppException(
+          message: 'This name of category exist $name',
+          errorType: ExceptionType.duplicateKeyException);
+    }
+  }
+
+  Future<Category> updateCategory(Session session,
+      {required int id, required String name, required String desc}) async {
+    final int? userId = await session.auth.authenticatedUserId;
+    if (userId == null) {
+      throw AppException(
+          message: 'This request required authintification of admin',
+          errorType: ExceptionType.authenticationRequiredException);
+    }
+    Category? category = await getCategoryById(session, id);
+    category.name = name.trim();
+    category.desc = desc.trim();
+    await checkCategoryByName(session, name);
+    category = await Category.db.updateRow(session, category);
+    return category;
+  }
+
   Future<Category> createCategory(Session session,
       {required String name, required String desc}) async {
     final int? userId = await session.auth.authenticatedUserId;
@@ -38,15 +87,7 @@ class CategoryEndpoint extends Endpoint {
           message: 'Only admin user can create category',
           errorType: ExceptionType.unauthorizedAccessException);
     }
-    final existOrNot = await Category.db.findFirstRow(
-      session,
-      where: (cat) => cat.name.ilike(name.trim()),
-    );
-    if (existOrNot != null) {
-      throw AppException(
-          message: 'This name of category exist',
-          errorType: ExceptionType.duplicateKeyException);
-    }
+    await checkCategoryByName(session, name);
     final Category createdCategory = await Category.db.insertRow(
         session, Category(name: name.trim(), desc: desc, userId: userId));
     return createdCategory;
@@ -65,13 +106,8 @@ class CategoryEndpoint extends Endpoint {
           message: 'Only admin user can delete categroy',
           errorType: ExceptionType.unauthorizedAccessException);
     }
-    final existOrNot = await Category.db.findById(session, id);
-    if (existOrNot == null) {
-      throw AppException(
-          message: 'This category not  found',
-          errorType: ExceptionType.notFound);
-    }
-    await Category.db.deleteRow(session, existOrNot);
-    return 'Category deleted successfully = ${existOrNot.toString()}';
+    final category = await getCategoryById(session, id);
+    await Category.db.deleteRow(session, category);
+    return 'Category deleted successfully = ${category.toString()}';
   }
 }
