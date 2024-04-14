a4a0fd55d5ab25c7be39b6e86eb36fb0cc20bf76
ihebmeftah <ihebmeftah666@gmail.com>
2024-04-14 15:19:03 +0100

    Add url_strategy package and set path strategy in main.dart
    Initialize url_strategy package and set path strategy in main.dart for better
    URL handling and routing. This change ensures proper URL management in the
    application.

--- lms_flutter/lib/main.dart
+++ lms_flutter/lib/main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'app/core/themes/themes.dart';
 import 'app/routes/app_pages.dart';
 import 'client_services.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Get.putAsync(() async => ClientServices());
+  setPathUrlStrategy();
   await ScreenUtil.ensureScreenSize();
   runApp(
     ScreenUtilInit(
